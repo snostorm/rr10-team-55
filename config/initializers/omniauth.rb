b6fceb7ed0b8045b2ba1d615317c09cc28ed475a
Chris Brulak <ccmbrulak@yahoo.ca>
2010-10-16 13:34:52 -0600

    removing unneeded provide options

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,8 +6,6 @@ Rails.application.config.middleware.use Rack::Session::Cookie # OmniAuth require
 #Rails.application.config.middleware.use OmniAuth::Strategies::GoogleApps, OpenID::Store::Filesystem.new('/tmp')
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, "rnwrrBWWzqFDJ2HbIxXU8A", "OTDHlKzxFcxqU0rRYFEzVZQIe5EWpZJbufcluBjqiQ"
-  provider :open_id, OpenID::Store::Filesystem.new('/tmp')
-  provider :open_id, OpenID::Store::Filesystem.new('/tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-  provider :open_id, OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com'  
+  provider :open_id, OpenID::Store::Filesystem.new('/tmp') 
   #provider :facebook, "APP_ID", "APP_SECRET"
 end
