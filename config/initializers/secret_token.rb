e301bb8409d08dad4aeb3b7f2e85a2ad5cc6c8ad
Sean Ouimet <sean@seanouimet.com>
2010-10-15 22:25:52 -0600

    Update to Rails 3.0.1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Letitfreeme::Application.config.secret_token = 'ce1be1c0e9cc01d3477ad89779ba870fdbb4c71b1f50ea777636aa8d2548d29a78ea0aeedd2ee1598cf20d16f2b4009ec4921f08f0b1f846308bcea7f286e6ce'
+Letitfreeme::Application.config.secret_token = '9b80a45579a9dfe0324c9addc08c4f8d82fd61fb23b8b40751dbb7067bafba95e74be210cda4ea53e40692eab02509fb169d5fbe940f5bf15e2a902f0d7702b3'
