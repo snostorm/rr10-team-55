fcd46238df6cb4e59c19a5afb1e2c92474549ac8
Chris Brulak <ccmbrulak@yahoo.ca>
2010-10-15 22:01:03 -0600

    moving to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letitfreeme::Application.config.secret_token = 'ce1be1c0e9cc01d3477ad89779ba870fdbb4c71b1f50ea777636aa8d2548d29a78ea0aeedd2ee1598cf20d16f2b4009ec4921f08f0b1f846308bcea7f286e6ce'
