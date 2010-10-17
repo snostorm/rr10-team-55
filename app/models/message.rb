6de856549f88f6ee19c2c8a7470869e9c497fdf6
Sean Ouimet <sean@seanouimet.com>
2010-10-17 10:47:51 -0600

    Adds user/message sent/received relations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,4 @@
 class Message < ActiveRecord::Base
-  #belongs_to :user, :through => :sender_id
-  #belongs_to :user, :through => :recipient_id
-
+  belongs_to :sender, :class_name=>'User', :foreign_key => "sender_id"
+  belongs_to :receipient, :class_name=>'User', :foreign_key => "receipient_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
   has_many :authorizations
   has_many :postings
   is_gravtastic
-  has_many :messages
   
+  has_many :sent_messages, :class_name=>'Message', :foreign_key => "sender_id"
+  has_many :received_message, :class_name=>'Message', :foreign_key => "recipient_id"
+
   cattr_reader :per_page
   @@per_page = 32
   
