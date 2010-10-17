e5a16a5628b94dd0eed3ec2c5fb4e1526a04c3b7
Sean Ouimet <sean@seanouimet.com>
2010-10-17 10:49:40 -0600

    Fixes pluralization on last association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   is_gravtastic
   
   has_many :sent_messages, :class_name=>'Message', :foreign_key => "sender_id"
-  has_many :received_message, :class_name=>'Message', :foreign_key => "recipient_id"
+  has_many :received_messages, :class_name=>'Message', :foreign_key => "recipient_id"
 
   cattr_reader :per_page
   @@per_page = 32
